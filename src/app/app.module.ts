f256727139d438a269481aed8b5b12d9168d927e
pavan1645 <pavan1645@gmail.com>
2018-03-05 21:26:10 +0530

    Routing and gallery file structure completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,31 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 import { MainService } from './main.service';
+import { RouterModule, Routes } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+import { RoutingComponent } from './routing/routing.component';
+
+const appRoutes: Routes = [
+	{ path: 'gallery', component: GalleryComponent },
+	{path: '**', component: AppComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    HttpModule,
-	BrowserModule,
-	NgbModule.forRoot(),
-	FormsModule
-  ],
-  providers: [MainService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		GalleryComponent,
+		RoutingComponent
+	],
+	imports: [
+		RouterModule.forRoot(
+			appRoutes,
+		),
+		HttpModule,
+		BrowserModule,
+		NgbModule.forRoot(),
+		FormsModule
+	],
+	providers: [MainService],
+	bootstrap: [RoutingComponent]
 })
 export class AppModule { }
